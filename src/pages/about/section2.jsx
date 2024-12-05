c3facacb8b111fcfae0c8b688a726eca1207f2b8
Saif <saifrafaat99@gmail.com>
2024-12-05 03:13:38 +0200

    about

--- src/pages/about/section2.jsx
+++ src/pages/about/section2.jsx
@@ -3,20 +3,20 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import img1 from '../../assets/factory1.jpg';
-import img2 from '../../assets/factory2.jpg';
-import img3 from '../../assets/factory6.jpg';
-import img4 from '../../assets/factory4.jpg';
-import img5 from '../../assets/factory5.jpg';
-import img6 from '../../assets/factory8.jpg';
-import img7 from '../../assets/factory9.jpg';
+import img1 from '../../assets/1.jpg';
+import img2 from '../../assets/2.jpg';
+import img3 from '../../assets/3.jpg';
+import img4 from '../../assets/5.jpg';
+import img5 from '../../assets/4.jpg';
+import img6 from '../../assets/7.jpg';
+import img7 from '../../assets/8.jpg';
 
 const Section2 = () => {
   const settings = {
     dots: false,
     infinite: true,
     speed: 1000,
-    slidesToShow: 5, // عرض 5 صور على الشاشات الكبيرة
+    slidesToShow: 3, // عرض 5 صور على الشاشات الكبيرة
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
@@ -38,13 +38,13 @@ const Section2 = () => {
       <div className="w-full ">
         <Slider {...settings}>
           {[img1, img2, img3, img4, img5, img6, img7].map((img, index) => (
-            <div key={index} className="px-1 pb-10"> {/* مسافة بين الصور */}
+            <div key={index} className="sm:px-8 pb-10"> {/* مسافة بين الصور */}
               <img 
                 src={img} 
                 alt={`Slide ${index + 1}`} 
                 className="w-full rounded-lg
-                           h-[200px] md:h-[200px] lg:h-[450px]
-                           object-cover transform transition-transform duration-500"
+                           h-[300px] md:h-[200px] lg:h-[450px]
+                          object-fill  sm:object-cover transform transition-transform duration-500"
               />
             </div>
           ))}
