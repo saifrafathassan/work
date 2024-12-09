6e43805ea283f1ed0bbc1e36caee5f5d68518969
Saif <saifrafaat99@gmail.com>
2024-12-10 01:43:49 +0200

    المنتجات

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -9,45 +9,47 @@ import Slider3 from '../../assets/slide3.webp';
 import Slider4 from '../../assets/slide4.webp';
 import Slider5 from '../../assets/slide5.webp';
 import Slider6 from '../../assets/slide6.webp';
-
+import i18n from 'i18next';
 import { useTranslation } from 'react-i18next';
 
-const products = [
-  {
-    category: 'Gold',
-    items: [
-      { image: Slider1, title: 'Gold', description: 'هنا هيبقي تفاصيل المنتج' },
-      { image: Slider2, title: 'Gold', description: 'هنا هيبقي تفاصيل المنتج' },
-    ],
-  },
-  {
-    category: 'Silver',
-    items: [
-      { image: Slider3, title: 'Silver', description: 'هنا هيبقي تفاصيل المنتج' },
-      { image: Slider4, title: 'Silver', description: 'هنا هيبقي تفاصيل المنتج' },
-    ],
-  },
-  {
-    category: 'Diamond',
-    items: [
-      { image: Slider5, title: 'Diamond', description: 'هنا هيبقي تفاصيل المنتج' },
-      { image: Slider6, title: 'Diamond', description: 'هنا هيبقي تفاصيل المنتج' },
-    ],
-  },
-];
 
 const Products = () => {
   const { t } = useTranslation();
+  const currentLanguage = i18n.language || i18n.options.fallbackLng[0];
+
+  const products = [
+    {
+      category: 'Gold',
+      items: [
+        { image: Slider1, title: '50x60 المقاس', description: t("info") },
+        { image: Slider2, title: '50x60 المقاس', description: t("info") },
+      ],
+    },
+    {
+      category: 'Silver',
+      items: [
+        { image: Slider3, title: '50x60 المقاس', description: t("info") },
+        { image: Slider4, title: '50x60 المقاس', description: t("info") },
+      ],
+    },
+    {
+      category: 'Diamond',
+      items: [
+        { image: Slider5, title: '50x60 المقاس', description: t("info") },
+        { image: Slider6, title: '50x60 المقاس', description: t("info") },
+      ],
+    },
+  ];
 
   return (
     <>
       <Navbar />
       <Headerps name={t('Products')} Carbg={Bg} />
-      <div className="container mx-auto py-20 w-full">
+      <div dir={currentLanguage === 'ar' ? 'rtl' : 'ltr'} className="container mx-auto py-20 w-full">
   {products.map((category, index) => (
     <div key={index} className="sm:mb-32 mb-10">
       {/* عنوان القسم */}
-      <h2 className="text-3xl sm:text-5xl text-main text-center font-bold mb-24">
+      <h2 className="text-3xl sm:text-6xl text-main text-center font-bold mb-24">
         {t(category.category)}
       <div className="w-[150px] h-1 bg-main mx-auto mt-4"></div> {/* السطر تحت العنوان */}
       </h2>
@@ -63,13 +65,14 @@ const Products = () => {
             <img
               src={product.image}
               alt={product.title}
-              className="w-full h-full border-b-2 pb-5 border-main object-contain  mb-6 " // تكبير الصورة
+              className="w-full h-full object-contain" // تكبير الصورة
             />
+                  <div className="w-[350px] h-1 bg-main mx-auto my-4"></div> {/* السطر تحت العنوان */}
             {/* تفاصيل المنتج */}
             <h3 className="text-2xl font-semibold text-center mb-4">
               {product.title}
             </h3>
-            <p className="text-gray-600 text-center mb-6 text-lg">
+            <p className="text-gray-600 text-center mb-6 text-lg mx-10">
               {product.description}
             </p>
 
